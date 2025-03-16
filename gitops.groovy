2b69afe4515efaadb076854609b834d55aa6bbca
Rohitmali9293 <rohitmali3010@gmail.com>
2025-03-16 19:21:18 +0530

    added json read config

--- gitops.groovy
+++ gitops.groovy
@@ -5,12 +5,31 @@ pipeline {
         timestamps()
         ansiColor('xtream')
     }
-    parameters {}
-    eanviroments{}
+    eanviroments{
+        PROJECT_ID = "tf-9293-sandbox"
+    }
     stages {
-        stage{
+        stage ('Project info checkout') {
             steps{
                 script{
+                    withCredentials([string(credentialsId: 'git-token1', variable: 'GIT_TOKEN')]) {
+                        echo 'clone the repository...'
+                        sh """
+                            rm -rf ${PROJECT_ID}
+                            git clone https://${GIT_TOKEN}@github.com/Rohitmali9293/${PROJECT_ID}.git
+                            cd ${DIR}/${PROJECT_ID}
+                            git submodule update --init --recursive --remote --force
+                            ls -l
+                        """
+                        def jsonFile = readFile('deploy.json')
+                        def jsonData = readJSON text: jsonFile
+                    // Extract values
+                        env.WORKSPACE = jsonData.workspace
+                        env.MODULE = jsonData.module
+                    // Print to verify
+                        echo "Workspace: ${env.WORKSPACE}"
+                        echo "Module: ${env.MODULE}"
+                    }
                 }
             }
         }
